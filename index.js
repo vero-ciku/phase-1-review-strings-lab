4a014701801dee212bfb82d3e1b20c89cf170499
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-06 11:26:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Norah';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, N{currentUser.slice(0, 1)}!`;
